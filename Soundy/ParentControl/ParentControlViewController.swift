401129b8a11da384714bfc8c4e03f875d39521e0
Reon0429-cat <oonishireon110@gmail.com>
2021-12-13 13:23:43 +0900

    ファイル追加

---
+++ Soundy/ParentControl/ParentControlViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ParentControlViewController.swift
+//  Soundy
+//
+//  Created by 大西玲音 on 2021/12/13.
+//
+
+import UIKit
+
+class ParentControlViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
