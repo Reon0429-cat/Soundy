07c851351cf5654f6cd7ebbda0c2429273204704
Reon0429-cat <oonishireon110@gmail.com>
2021-12-15 20:31:01 +0900

    no message

---
+++ Soundy/Parent/MusicSourceControl/MusicSourceControlCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MusicSourceControlCollectionViewCell.swift
+//  Soundy
+//
+//  Created by 大西玲音 on 2021/12/15.
+//
+
+import UIKit
+
+class MusicSourceControlCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
